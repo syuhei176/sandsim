7f24e649cbd43c3eba232fc3999a4c7f6be9aac7
syuhei176 <syuhei176@gmail.com>
2022-03-15 01:38:38 +0900

    updated

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const width = 320
-const height = 320
+const EMPTY = 0
+const SAND = 1
+const WATER = 2
+const SOIL = 3
+const LAVA = 4
+
+const width = 300
+const height = 300
 let stage = initStage()
 let isMouseDown = false
-let selectedPowderType = 1
+let selectedPowderType = SAND
+
 
 function initStage() {
   const stage = []
   for (let i = 0; i < width; i++) {
-    stage[i] = []
-    for (let j = 0; j < height; j++) {
-      stage[i][j] = 0
-    }
+    stage[i] = Array(height)
+    stage[i].fill(EMPTY)
   }
   return stage
 }
@@ -22,44 +27,90 @@ function putPowder(x, y, powder) {
 
 function updatePowder(i, j, stage, nextStage, powder, maxP) {
   if (j < height) {
+    if ((stage[i][j] === WATER && stage[i][j + 1] === LAVA) || (
+      stage[i][j + 1] === WATER && stage[i][j] === LAVA
+    )) {
+      nextStage[i][j] = SAND
+      nextStage[i][j + 1] = SAND
+      return
+    }
+
     for (let p = 0; p < maxP; p++) {
-      if (stage[i + p][j + 1] === 0) {
+      if (i + p >= width || j + p >= height) break
+      if (i - p < 0 || j - p < 0) break
+
+      if (stage[i + p][j + 1] === EMPTY) {
         nextStage[i + p][j + 1] = powder
         return
-      } else if (stage[i - p][j + 1] === 0) {
+      }
+
+      if (powder !== WATER) {
+        if (stage[i + p][j + 1] === WATER) {
+          nextStage[i + p][j + 1] = powder
+          nextStage[i][j] = WATER
+          return
+        }
+      }
+
+      if (stage[i + p][j + 1] !== EMPTY && stage[i + p][j + 1] !== powder) {
+        break
+      }
+    }
+
+    for (let p = 0; p < maxP; p++) {
+      if (i + p >= width || j + p >= height) break
+      if (i - p < 0 || j - p < 0) break
+
+      if (stage[i - p][j + 1] === EMPTY) {
         nextStage[i - p][j + 1] = powder
         return
       }
+
+      if (powder !== WATER) {
+        if (stage[i - p][j + 1] === WATER) {
+          nextStage[i - p][j + 1] = powder
+          nextStage[i][j] = WATER
+          return
+        }
+      }
+
+      if (stage[i - p][j + 1] !== EMPTY && stage[i - p][j + 1] !== powder) {
+        break
+      }
     }
-    nextStage[i][j] = powder
+
+    if (nextStage[i][j] === EMPTY) {
+      nextStage[i][j] = powder
+    }
   }
 }
 
-function update() {
+function update(stage) {
   const nextStage = initStage()
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
-      // 砂
-      if (stage[i][j] === 1) {
-        updatePowder(i, j, stage, nextStage, 1, 2)
+      let viscosity = 0
+      if (stage[i][j] === SAND) {
+        viscosity = 2
+      } else if (stage[i][j] === WATER) {
+        viscosity = 20
+      } else if (stage[i][j] === SOIL) {
+        viscosity = 1
+      } else if (stage[i][j] === LAVA) {
+        viscosity = 5
       }
-      // 水
-      else if (stage[i][j] === 2) {
-        updatePowder(i, j, stage, nextStage, 2, 5)
-      }
-      // 土
-      else if (stage[i][j] === 3) {
-        updatePowder(i, j, stage, nextStage, 3, 1)
+      if (viscosity > 0) {
+        updatePowder(i, j, stage, nextStage, stage[i][j], viscosity)
       }
     }
   }
-  stage = nextStage
+  return nextStage
 }
 
 function draw(stage) {
-  const w = width * 2
-  const h = height * 2
   const size = 2
+  const w = width * size
+  const h = height * size
 
   var canvas = document.getElementById('sample');
   if (canvas.getContext) {
@@ -71,32 +122,37 @@ function draw(stage) {
         const powder = stage[i][j]
 
         const indexes = [
-          4 * (2 * i + 2 * j * w),
-          4 * ((2 * i + 1) + 2 * j * w),
-          4 * (2 * i + (2 * j + 1) * w),
-          4 * ((2 * i + 1) + (2 * j + 1) * w),
+          4 * (size * i + size * j * w),
+          4 * ((size * i + 1) + size * j * w),
+          4 * (size * i + (size * j + 1) * w),
+          4 * ((size * i + 1) + (size * j + 1) * w),
         ]
 
         for (let index of indexes) {
-          if (powder === 0) {
+          if (powder === EMPTY) {
             imgData.data[index] = 50
             imgData.data[index + 1] = 50
             imgData.data[index + 2] = 50
             imgData.data[index + 3] = 255
-          } else if (powder === 1) {
+          } else if (powder === SAND) {
             imgData.data[index] = 200
             imgData.data[index + 1] = 180
             imgData.data[index + 2] = 100
             imgData.data[index + 3] = 255
-          } else if (powder === 2) {
-            imgData.data[index] = 150
-            imgData.data[index + 1] = 150
-            imgData.data[index + 2] = 200
+          } else if (powder === WATER) {
+            imgData.data[index] = 120
+            imgData.data[index + 1] = 120
+            imgData.data[index + 2] = 210
             imgData.data[index + 3] = 255
-          } else {
+          } else if (powder === SOIL) {
             imgData.data[index] = 100
             imgData.data[index + 1] = 100
             imgData.data[index + 2] = 100
+            imgData.data[index + 3] = 255
+          } else {
+            imgData.data[index] = 200
+            imgData.data[index + 1] = 70
+            imgData.data[index + 2] = 70
             imgData.data[index + 3] = 255
           }
         }
@@ -111,21 +167,25 @@ function draw(stage) {
 window.onload = function () {
   const selected = document.getElementById('selected')
   document.getElementById('sand').onclick = function () {
-    selectedPowderType = 1
+    selectedPowderType = SAND
     selected.innerText = 'Sand'
   }
   document.getElementById('water').onclick = function () {
-    selectedPowderType = 2
+    selectedPowderType = WATER
     selected.innerText = 'Water'
   }
   document.getElementById('soil').onclick = function () {
-    selectedPowderType = 3
+    selectedPowderType = SOIL
     selected.innerText = 'soil'
+  }
+  document.getElementById('lava').onclick = function () {
+    selectedPowderType = LAVA
+    selected.innerText = 'lava'
   }
 
 
   function updateScreen(time) {
-    update()
+    stage = update(stage)
     draw(stage)
     requestAnimationFrame(updateScreen);
   }
